a3f8c26f9e90a28d6e605916062684ffee6dbb59
Julio <j.tenciocoto@gmail.com>
2023-05-12 20:43:01 -0600

    added jumpbox vm

--- my-azure-infra/aks_cluster/main.tf
+++ my-azure-infra/aks_cluster/main.tf
@@ -60,7 +60,129 @@ resource "azurerm_kubernetes_cluster_node_pool" "user" {
   vnet_subnet_id        = azurerm_subnet.aks-default-subnet.id
 }
 
-#todo --- add block to create Jumpbox VMs
+#todo --- add block to create Jumpbox VM
+resource "azurerm_resource_group" "jumpbox-rg" {
+  name     = var.jumpbox_resource_group
+  location = var.jumpbox_location
+}
+resource "azurerm_virtual_network" "jumpbox" {
+  name                = var.jumpbox_vnet_name
+  address_space       = var.jumpbox_address_space
+  location            = azurerm_resource_group.jumpbox-rg.location
+  resource_group_name = azurerm_resource_group.jumpbox-rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.jumpbox-rg.name
+  virtual_network_name = azurerm_virtual_network.jumpbox.name
+  address_prefixes     = var.jumpbox_address_prefixes
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.jumpbox-rg.location
+  resource_group_name = azurerm_resource_group.jumpbox-rg.name
+  allocation_method   = var.ip_allocation_method
+  tags                = var.tags
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.jumpbox-rg.location
+  resource_group_name = azurerm_resource_group.jumpbox-rg.name
+
+  security_rule {
+    name                       = var.nsg_rule_name
+    priority                   = var.nsg_rule_priority
+    direction                  = var.nsg_rule_direction
+    access                     = var.nsg_rule_access
+    protocol                   = var.nsg_rule_protocol
+    source_port_range          = var.nsg_rule_source_port_range
+    destination_port_range     = var.nsg_rule_destination_port_range
+    source_address_prefix      = var.nsg_rule_source_address_prefix
+    destination_address_prefix = var.nsg_rule_destination_address_prefix
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface" "jumpbox-nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.jumpbox-rg.location
+  resource_group_name = azurerm_resource_group.jumpbox-rg.name
+
+  ip_configuration {
+    name                          = var.ipconfig_name
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = var.private_ip_allocation
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.jumpbox-nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.jumpbox-rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "diag${random_id.randomId.hex}"
+  resource_group_name      = azurerm_resource_group.jumpbox-rg.name
+  location                 = azurerm_resource_group.jumpbox-rg.location
+  account_tier             = var.sa_account_tier
+  account_replication_type = var.sa_account_replication_type
+  tags                     = var.tags
+}
+
+resource "azurerm_linux_virtual_machine" "jumpbox" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.jumpbox-rg.location
+  resource_group_name   = azurerm_resource_group.jumpbox-rg.name
+  network_interface_ids = [azurerm_network_interface.jumpbox-nic.id]
+  size                  = var.vm_size
+
+  os_disk {
+    name                 = var.os_disk_name
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_storage_account_type
+  }
+
+  source_image_reference {
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.os_version
+  }
+
+  computer_name                   = var.computer_name
+  admin_username                  = var.admin_username
+  disable_password_authentication = var.disable_password_auth
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.public_key
+
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
+  }
+
+  tags = var.tags
+}
 
 
 #todo --- add block to peer the Jumpbox VNET to the AKS VNET
