fe27433ab3d301b55bbdf660b89574749fe17d7a
Julio <j.tenciocoto@gmail.com>
2023-05-12 20:58:22 -0600

    moved the vnet/subnet to node rg

--- my-azure-infra/aks_cluster/main.tf
+++ my-azure-infra/aks_cluster/main.tf
@@ -7,12 +7,12 @@ resource "azurerm_virtual_network" "aks-vnet" {
   name                = var.vnet_name
   address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.node_resource_group
 }
 
 resource "azurerm_subnet" "aks-default-subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.node_resource_group
   virtual_network_name = azurerm_virtual_network.aks-vnet.name
   address_prefixes     = var.address_prefixes
 }
