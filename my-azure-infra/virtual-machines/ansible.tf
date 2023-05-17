f6722c410474ee94c02676daf16b9beda2535ef5
Julio <j.tenciocoto@gmail.com>
2023-05-17 13:55:52 -0600

    changed anisble hostname ot use varible

--- my-azure-infra/virtual-machines/ansible.tf
+++ my-azure-infra/virtual-machines/ansible.tf
@@ -1,15 +1,19 @@
+resource "ansible_group" "webservers" {
+  name = "webservers"
+}
 resource "ansible_host" "nginx-vm" {
   depends_on = [azurerm_linux_virtual_machine.linux-vm]
   name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
-  groups     = ["webservers"]
+  groups     = [ansible_group.webservers.name]
   variables = {
     ansible_user               = var.admin_username
     ansible_ssh_private_key    = "~/.ssh/id_rsa"
     ansible_python_interpreter = "/usr/bin/python3"
   }
 }
-resource "ansible_playbook" "nginx" {
-  playbook   = "playbook.yml"
-  name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
-  replayable = true
+resource "ansible_playbook" "playbook" {
+  playbook                = "playbook.yml"
+  ansible_playbook_binary = "/opt/homebrew/bin/ansible-playbook"
+  name                    = azurerm_linux_virtual_machine.linux-vm.public_ip_address
+  replayable              = true
 }
