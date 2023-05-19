80bd95b941fda719a0b5004e3b27dd4761732aa4
Julio <j.tenciocoto@gmail.com>
2023-05-18 18:21:02 -0600

    removing useless parameters

--- my-azure-infra/virtual-machines/ansible.tf
+++ my-azure-infra/virtual-machines/ansible.tf
@@ -5,15 +5,10 @@ resource "ansible_host" "nginx-vm" {
   depends_on = [azurerm_linux_virtual_machine.linux-vm]
   name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
   groups     = [ansible_group.webservers.name]
-  variables = {
-    ansible_user               = var.admin_username
-    ansible_ssh_private_key    = "~/.ssh/id_rsa"
-    ansible_python_interpreter = "/usr/bin/python3"
-  }
 }
 resource "ansible_playbook" "playbook" {
-  playbook                = "playbook.yml"
-  ansible_playbook_binary = "/opt/homebrew/bin/ansible-playbook"
-  name                    = azurerm_linux_virtual_machine.linux-vm.public_ip_address
-  replayable              = true
+  depends_on = [azurerm_linux_virtual_machine.linux-vm]
+  playbook   = "playbook.yml"
+  name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
+  replayable = true
 }
