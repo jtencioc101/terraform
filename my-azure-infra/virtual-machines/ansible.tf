a1cb9c2d6e36b49d33649f36bac80dca0e43c892
Julio <j.tenciocoto@gmail.com>
2023-05-19 11:04:04 -0600

    removed unnecesary group statement

--- my-azure-infra/virtual-machines/ansible.tf
+++ my-azure-infra/virtual-machines/ansible.tf
@@ -1,6 +1,3 @@
-resource "ansible_group" "webservers" {
-  name = "webservers"
-}
 resource "ansible_host" "nginx-vm" {
   depends_on = [azurerm_linux_virtual_machine.linux-vm]
   name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
