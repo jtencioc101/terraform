f73dbb9f0a5b6571a6767eb49b37721a0abb146c
Julio <j.tenciocoto@gmail.com>
2023-05-19 12:19:40 -0600

    added ansible.tf

---
+++ my-azure-infra/aks_cluster/ansible.tf
@@ -0,0 +1,10 @@
+resource "ansible_host" "aks_jumpbox" {
+  depends_on = [azurerm_linux_virtual_machine.jumpbox]
+  name       = azurerm_linux_virtual_machine.jumpbox.public_ip_address
+
+}
+resource "ansible_playbook" "playbook" {
+  playbook   = "playbook.yml"
+  name       = azurerm_linux_virtual_machine.jumpbox.public_ip_address
+  replayable = true
+}
