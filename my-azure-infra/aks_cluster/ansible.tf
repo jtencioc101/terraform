79d6d1e14c4f993cbfc20fb6041d848bd6c66bbc
Julio <j.tenciocoto@gmail.com>
2023-05-19 16:29:25 -0600

    adding playbook dependency to cluster creation

--- my-azure-infra/aks_cluster/ansible.tf
+++ my-azure-infra/aks_cluster/ansible.tf
@@ -4,6 +4,7 @@ resource "ansible_host" "aks_jumpbox" {
 
 }
 resource "ansible_playbook" "playbook" {
+  depends_on = [azurerm_kubernetes_cluster.aks]
   playbook   = "playbook.yml"
   name       = azurerm_linux_virtual_machine.jumpbox.public_ip_address
   replayable = true
