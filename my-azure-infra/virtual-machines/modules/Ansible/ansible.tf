e65fc5268f8ad86283fcb7e1b12e7bdf288c3053
Julio <j.tenciocoto@gmail.com>
2023-05-20 10:38:20 -0600

    added provider block

--- my-azure-infra/virtual-machines/modules/Ansible/ansible.tf
+++ my-azure-infra/virtual-machines/modules/Ansible/ansible.tf
@@ -1,3 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+
+    ansible = {
+      version = "~> 1.1.0"
+      source  = "ansible/ansible"
+    }
+  }
+  cloud {
+    organization = "jct-terratest"
+    workspaces {
+      name = "compute_resources"
+    }
+  }
+}
+
 resource "ansible_host" "nginx-vm" {
   depends_on = [azurerm_linux_virtual_machine.linux-vm]
   name       = azurerm_linux_virtual_machine.linux-vm.public_ip_address
